0dfc7eed37643e0f9bd13919fea6a5b07be055b9
Darryl Yeo <dev@darryl-yeo.com>
2020-08-30 06:41:21 +0000

    Ensure latest Deno

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-# Run Rust interpreter
-# cargo run
+# (Ensure the latest Deno)
+[ -x ~/.deno/bin/deno ] || curl -fsSL https://deno.land/x/install/install.sh | sh > /dev/null
+alias deno="~/.deno/bin/deno"
 
 # Run Deno interpreter
 deno run --allow-read --allow-env --unstable src/main.ts
+
+# -----
+
+# Run Rust interpreter
+# cargo run
